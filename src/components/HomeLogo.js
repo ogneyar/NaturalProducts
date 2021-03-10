351c3c2444c5d8211d234f8fb74ccdf1f2a301f0
ogneyar <ogneyar-ne@ya.ru>
2021-03-10 15:04:08 +0300

    add logo

--- src/components/HomeLogo.js
+++ src/components/HomeLogo.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import logo from '../image/logo.png'
+// import logo from '../image/ring.png'
+import logo1 from '../image/ring1.png'
+import logo2 from '../image/ring2.jpg'
 import '../styles/HomeLogo.scss'
 
 const HomeLogo = () => {
     return (
-        <div className="Home-logo">
-            <img src={logo} alt=""/>
+        <div className="container-Home-logo">
+            <header></header>
+            <div className="Home-logo">
+                {/* <img className="logo" src={logo} alt=""/> */}
+                <img className="logo" src={logo1} alt=""/>
+                <img className="logo-fon" src={logo2} alt=""/>
+                <h1>приРОДные ПРОДУКТЫ</h1>
+            </div>
+            <footer>
+                <h3>ПриродныеПродукты.рус</h3>
+              
+                <p>2021</p>
+            </footer>
         </div>
     )
 }
