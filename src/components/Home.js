97f70805ff5dc20635ff02840089a9991dff7b9f
ogneyar <ogneyar-ne@ya.ru>
2021-03-12 18:00:24 +0300

    add houseKiprey files

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,21 +9,31 @@ import ekoMir from '../image/ekoMir.png';
 
 const Home = () => {
     return (
-    <div className="container-home">
+    <div>
         <Header />
         <Logo />
         <SideBar />
         <main className="main">
-            <div className="main-img">
-                <img src={ekoMir} alt="ekoMir" />
-            </div>
-            <div className="main-block">
-                <a href="/EkoMir1.html">Прайс ЭкоМир Чаи</a>
-                <a href="/EkoMir2.html">Прайс ЭкоМир Масла</a>
-                <a href="/EkoMir3.html">Прайс ЭкоМир Мази</a>
-                <a href="/EkoMir4.html">Прайс ЭкоМир Травы</a>
-            </div>
+            <article className="main-container">
+                <div className="main-img">
+                    <img src={ekoMir} alt="ekoMir" />
+                </div>
+                <div className="main-block">
+                    <a href="/EkoMir1.html">Прайс ЭкоМир Чаи</a>
+                    <a href="/EkoMir2.html">Прайс ЭкоМир Масла</a>
+                    <a href="/EkoMir3.html">Прайс ЭкоМир Мази</a>
+                    <a href="/EkoMir4.html">Прайс ЭкоМир Травы</a>
+                </div>
+            </article>
+            <article className="main-container">
+                <div className="main-inline-block">
+                    <a href="https://ДомКипрея.рус">🏡Дом Кипрея🌿</a>
+                    <a href="/HouseKiprey1.html">продукция из т.к.🌱</a>
+                </div>
+            </article>
+
         </main>
+
         <Footer />
     </div>
     );
