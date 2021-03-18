b9f63a6c0ad9cf7e8c5ba4b9ba89aac7600ed59b
ogneyar <ogneyar-ne@ya.ru>
2021-03-18 20:24:59 +0300

    qqq

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import '../styles/home.scss';
 import ekoMir from '../image/ekoMir.png';
 import divnolar from '../image/divnolar.jpg';
 import hutor from '../image/hutor.png';
+// eslint-disable-next-line
 import whiteList from '../image/white_list.jpg';
 import domKipreya from '../image/dom_kipreya.png';
 
@@ -19,28 +20,42 @@ const Home = () => {
         <Logo />
         <SideBar />
         <main className="main">
-            <section className="main-container">
-                <h2>Продукция наших мастеров</h2>
+            <section className="main-container">                
+                <h2 className="main-container__title">Продукция наших мастеров</h2>
             </section>
 
             <section className="main-container">
-                <div className="main-img border">
-                    <a href="https://ladaga.ru"><img src={ekoMir} alt="ekoMir" /></a>
+                <div className="main-img">
+                    <a href="https://ladaga.ru"><img className="border" src={ekoMir} alt="ekoMir" /></a>
                 </div>
                 <div className="main-block">
-                    <a href="/EkoMir1.html">Прайс ЭкоМир Чаи</a>
-                    <a href="/EkoMir2.html">Прайс ЭкоМир Масла</a>
-                    <a href="/EkoMir3.html">Прайс ЭкоМир Мази</a>
-                    <a href="/EkoMir4.html">Прайс ЭкоМир Травы</a>
+
+                    <div className="main-block__url">
+                        <a href="/EkoMir1.html">Продукция ladaga.ru</a>
+                    </div>
+                    <div className="main-block__text">
+                        <p>Тут необходимо добавить описание.</p>
+                    </div>
+
+                    {/* <a href="/pages/EkoMir1.html">Прайс ЭкоМир Чаи</a>
+                    <a href="/pages/EkoMir2.html">Прайс ЭкоМир Масла</a>
+                    <a href="/pages/EkoMir3.html">Прайс ЭкоМир Мази</a>
+                    <a href="/pages/EkoMir4.html">Прайс ЭкоМир Травы</a> */}
+
                 </div>
             </section>
             
             <section className="main-container">
-                <div className="main-img border">
-                    <a href="https://ДомКипрея.рус"><img src={domKipreya} alt="domKipreya" /></a>
+                <div className="main-img">
+                    <a href="https://ДомКипрея.рус"><img className="border" src={domKipreya} alt="domKipreya" /></a>
                 </div>
                 <div className="main-block">
-                    <a href="/HouseKiprey1.html">продукция из т.к.🌱</a>
+                    <div className="main-block__url">
+                        <a href="/pages/HouseKiprey1.pdf">Продукция из т.к.🌱</a>
+                    </div>
+                    <div className="main-block__text">
+                        <p>Тут необходимо добавить описание.</p>
+                    </div>
                 </div>
             </section>
 
@@ -59,8 +74,8 @@ const Home = () => {
             </section>
 
             <section className="main-container">
-                <div className="main-img border">
-                    <a href="https://hutoryanin.ru"><img src={hutor} alt="hutor" /></a>
+                <div className="main-img">
+                    <a href="https://hutoryanin.ru"><img className="border" src={hutor} alt="hutor" /></a>
                 </div>
                 <div className="main-block">
                     <div className="main-block__url ">
@@ -78,16 +93,19 @@ const Home = () => {
 
             <section className="main-container">
                 <div className="main-textil">
-                    <p>Ремесленные наРОДные пРОДукты из Технической Конопли</p>
+                    <div className="main-textil__block">
+                        
+                        <h4>Ремесленные наРОДные пРОДукты из Технической Конопли</h4>
 
-                    <a href="images/textil1.jpg"><img src="images/textil1.jpg" alt="текстиль"/></a>
-                    <a href="images/textil2.jpg"><img src="images/textil2.jpg" alt="текстиль"/></a>
-                    <a href="images/textil3.jpg"><img src="images/textil3.jpg" alt="текстиль"/></a>
-                    <a href="images/textil4.jpg"><img src="images/textil4.jpg" alt="текстиль"/></a>
-                    <a href="images/textil5.jpg"><img src="images/textil5.jpg" alt="текстиль"/></a>
-                    <a href="images/textil6.jpg"><img src="images/textil6.jpg" alt="текстиль"/></a>
-                    <a href="images/textil7.jpg"><img src="images/textil7.jpg" alt="текстиль"/></a>
-                    <a href="images/textil8.jpg"><img src="images/textil8.jpg" alt="текстиль"/></a>
+                        <a href="images/textil1.jpg"><img src="images/textil1.jpg" alt="текстиль"/></a>
+                        <a href="images/textil2.jpg"><img src="images/textil2.jpg" alt="текстиль"/></a>
+                        <a href="images/textil3.jpg"><img src="images/textil3.jpg" alt="текстиль"/></a>
+                        <a href="images/textil4.jpg"><img src="images/textil4.jpg" alt="текстиль"/></a>
+                        <a href="images/textil5.jpg"><img src="images/textil5.jpg" alt="текстиль"/></a>
+                        <a href="images/textil6.jpg"><img src="images/textil6.jpg" alt="текстиль"/></a>
+                        <a href="images/textil7.jpg"><img src="images/textil7.jpg" alt="текстиль"/></a>
+                        <a href="images/textil8.jpg"><img src="images/textil8.jpg" alt="текстиль"/></a>
+                    </div>
                 </div>
             </section>
 
